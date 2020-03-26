9493b5e0f398a75f8fe0700fc6c5e6cd4f20a832
Elliot Redhead <elliot.redhead@gmail.com>
2020-03-26 16:49:10 +0000

    Configured static items.

--- printcrate/settings.py
+++ printcrate/settings.py
@@ -119,4 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_LOCATION = "static"
+STATICFILES_STORAGE = "custom_storages.StaticStorage"
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]

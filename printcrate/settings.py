e224642f89b1b254c247a893ab5bf6907320c537
Elliot Redhead <elliot.redhead@gmail.com>
2020-03-26 16:50:29 +0000

    Import env file if it exists.

--- printcrate/settings.py
+++ printcrate/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+if os.path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

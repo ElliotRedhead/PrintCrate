5f967c5a16a3825003d9ff58fa2a5a80cab07199
Elliot Redhead <elliot.redhead@gmail.com>
2020-03-27 23:21:00 +0000

    Removed index from accounts URLs.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

10c3042734f917556c02945f2f4f00c5836fb9c3
Elliot Redhead <elliot.redhead@gmail.com>
2020-03-27 18:46:35 +0000

    Created logout view.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
+from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+
+
+@login_required
+def logout(request):
+    """Log the user out."""
+    auth.logout(request)
+    messages.success(request, "You have successfully been logged out.")
+    return redirect(reverse("index"))

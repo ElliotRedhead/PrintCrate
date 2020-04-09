03be972ce1756d5c1f548ed885b6f4d0449623fe
Elliot Redhead <elliot.redhead@gmail.com>
2020-04-09 16:54:59 +0000

    Linked login template to URL.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . views import registration, logout
+from django.contrib.auth import views as auth
 
 urlpatterns = [
     path("logout", logout, name="logout"),
     path("register", registration, name="register"),
+    path("login", auth.LoginView.as_view(template_name="login.html", redirect_authenticated_user=True), name="login"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

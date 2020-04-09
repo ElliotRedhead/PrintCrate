179f951708606cda47ba822691f732e3dc331ecd
Elliot Redhead <elliot.redhead@gmail.com>
2020-04-09 16:24:06 +0000

    Linked registration through URL, with link in nav bar.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from . views import logout
+from . views import registration, logout
 
 urlpatterns = [
-    path("logout", logout, name="logout")
+    path("logout", logout, name="logout"),
+    path("register", registration, name="register"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

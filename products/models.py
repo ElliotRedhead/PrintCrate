17aa09563ecc3aef3194182094d1d0b26a721af7
unknown <elliot.redhead@gmail.com>
2020-04-12 14:14:24 +0100

    Created test product model.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class product(models.Model):
+	product_name=models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.product_name

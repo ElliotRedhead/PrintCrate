d513d943ccc82bbfe47e7b0087180ca1e947abd7
unknown <elliot.redhead@gmail.com>
2020-04-13 11:53:29 +0100

    Added required fields to products model.

--- products/models.py
+++ products/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    stock_available = models.PositiveSmallIntegerField(
+        validators=[MaxValueValidator(50)])
 
     def __str__(self):
         return self.name

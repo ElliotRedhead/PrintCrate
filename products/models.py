c4a3401ad55357a386c8c5df19547c7eed6d0544
unknown <elliot.redhead@gmail.com>
2020-04-15 21:19:14 +0100

    Added additional value validators.

--- products/models.py
+++ products/models.py
@@ -1,13 +1,14 @@
 from django.db import models
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Product(models.Model):
     name = models.CharField(max_length=25)
     description = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(
+        max_digits=5, decimal_places=2, validators=[MinValueValidator(0.00)])
     stock_available = models.PositiveSmallIntegerField(
-        validators=[MaxValueValidator(50)])
+        validators=[MinValueValidator(0), MaxValueValidator(50)])
 
     def __str__(self):
         return self.name

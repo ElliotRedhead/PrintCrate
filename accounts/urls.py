d685ae62177b8ccc927a93e2482635b2686815c3
unknown <elliot.redhead@gmail.com>
2020-04-18 12:07:46 +0100

    Added extra context variable to preset Login view.

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("logout", logout, name="logout"),
     path("register", registration, name="register"),
     path("login", auth.LoginView.as_view(template_name="login.html",
-                                         redirect_authenticated_user=True), name="login"),
+                                         redirect_authenticated_user=True, extra_context={"page_title": "Login | PrintCrate"}), name="login"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

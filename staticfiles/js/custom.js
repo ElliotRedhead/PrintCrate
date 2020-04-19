7118a669e041ae9371b74a427abc40f37fd60bee
unknown <elliot.redhead@gmail.com>
2020-04-19 19:35:15 +0100

    Removed redundant console log.

--- staticfiles/js/custom.js
+++ staticfiles/js/custom.js
@@ -1,6 +1,5 @@
 $(function () {
     const viewportRemaining = window.innerHeight - $("nav").outerHeight();
-    console.log($(".jumbotron").outerHeight(true))
     $(".jumbotron").css("margin-bottom", 0);
     $(".jumbotron").outerHeight(viewportRemaining);
 }

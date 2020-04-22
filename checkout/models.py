18f917cf9253b24b379e07a739e1a6a88f999b0b
unknown <elliot.redhead@gmail.com>
2020-04-22 19:22:44 +0100

    Constructed basic order/user details for shipping.

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from products.models import Product
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class OrderDetails(models.Model):
+    customer_auth = models.ForeignKey(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100, blank=False)
+    primary_address_line = models.CharField(max_length=50, blank=False)
+    secondary_address_line = models.CharField(
+        max_length=50, blank=True, null=True)
+    town_or_city = models.CharField(max_length=50, blank=False)
+    county = models.CharField(max_length=50, blank=False)
+    postcode = models.CharField(max_length=20, blank=False)
+    country = models.CharField(max_length=50, blank=False)

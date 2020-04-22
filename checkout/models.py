d65392007724922380d620cf4e7fbcee1c5a9704
unknown <elliot.redhead@gmail.com>
2020-04-22 22:39:21 +0100

    Added more requirements for order details model.

--- checkout/models.py
+++ checkout/models.py
@@ -13,3 +13,8 @@ class OrderDetails(models.Model):
     county = models.CharField(max_length=50, blank=False)
     postcode = models.CharField(max_length=20, blank=False)
     country = models.CharField(max_length=50, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
+    purchase_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.id}-{self.purchase_date}-{self.customer_auth.username}"

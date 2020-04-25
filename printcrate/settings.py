79d24dbccbab396d00d756810072b9a2c3ec1511
unknown <elliot.redhead@gmail.com>
2020-04-25 10:18:19 +0100

    Temporarily disabled conditional database select.

--- printcrate/settings.py
+++ printcrate/settings.py
@@ -83,16 +83,16 @@ WSGI_APPLICATION = 'printcrate.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-if "DATABASE_URL" in os.environ:
-    DATABASES = {"default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL"))}
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+# if "DATABASE_URL" in os.environ:
+DATABASES = {"default": dj_database_url.parse(
+    os.environ.get("DATABASE_URL"))}
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 
 
 # Password validation

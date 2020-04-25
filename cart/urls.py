e46c1c8ea4d4ab888e9f34513804da4af2cb4848
unknown <elliot.redhead@gmail.com>
2020-04-25 20:14:13 +0100

    Cart item quantities management from CI course.

--- cart/urls.py
+++ cart/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path
+from django.urls import path, include
+from .views import cart_view
 
 urlpatterns = [
-    path("", cart_view, name="cart_view")
+    path("", view_cart, name="view_cart"),
+    re_path(r"add/(?P<id>\d+)", add_to_cart, name="add_to_cart"),
+    re_path(r"adjust/(?P<id>\d+)", adjust_cart, name="adjust_cart"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
-# Create your views here.
+# The contents of this file are closely based on the course content taught in
+# the CodeInstitute Full Stack Web Development course.
+# The teachings have been modified for the project owner's development style
+# and to best suit the PrintCrate project.
+
+
+def cart_view(request):
+    return render(request, "cart.html")
+
+
+def add_to_cart(request, id):
+    quantity = int(request.POST.get("quantity"))
+
+    cart = request.session.get("cart", {})
+    if id in cart:
+        cart[id] = int(cart[id]) + quantity
+    else:
+        cart[id] = cart.get(id, quantity)
+
+    request.session["cart"] = cart
+    return redirect(reverse("cart_view"))
+
+
+def adjust_cart(request, id):
+    quantity = int(request.POST.get("quantity"))
+    cart = request.session.get("cart", {})
+
+    if quantity > 0:
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+
+    request.session["cart"] = cart
+    return redirect(reverse("cart_view"))

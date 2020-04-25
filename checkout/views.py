fb16526280a1e8ce45b779eea3d00da16f9cd863
unknown <elliot.redhead@gmail.com>
2020-04-25 12:11:42 +0100

    Added required form in context.

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .forms import CustomerShippingForm
 
 
 @login_required
 def checkout_shipping_address_view(request):
-    return render(request, "checkout_shipping_address.html", {"page_title": "Shipping | PrintCrate"})
+    return render(request, "checkout_shipping_address.html", {"page_title": "Shipping | PrintCrate", "customer_shipping_form": CustomerShippingForm})

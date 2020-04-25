d9b7629717aa6b36b2ba878eb7b5ee9e7f2a2bcf
unknown <elliot.redhead@gmail.com>
2020-04-25 23:47:30 +0100

    Corrected name of path.

--- cart/urls.py
+++ cart/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import cart_view
 
 urlpatterns = [
-    path("", view_cart, name="view_cart"),
+    path("", view_cart, name="cart_view"),
     re_path(r"add/(?P<id>\d+)", add_to_cart, name="add_to_cart"),
     re_path(r"adjust/(?P<id>\d+)", adjust_cart, name="adjust_cart"),
 ]

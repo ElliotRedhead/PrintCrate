39dc816dc9cb89c3bae4c93ecd9087349d16e03b
unknown <elliot.redhead@gmail.com>
2020-04-25 10:11:18 +0100

    Rendered shipping info to monitor development.

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def checkout_shipping_address_view(request):
+    return render(request, "checkout_shipping_address.html", {"page_title": "Shipping | PrintCrate"})

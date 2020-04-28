1742c7348a24f8f175c0fbe98a2048e6e201460d
unknown <elliot.redhead@gmail.com>
2020-04-28 20:37:05 +0100

    Created single-product display page.

--- products/views.py
+++ products/views.py
@@ -5,3 +5,8 @@ from .models import Product
 def products_view(request):
     products = Product.objects.all()
     return render(request, "productslist.html", {"products": products, "page_title": "Products | PrintCrate"})
+
+
+def product_detail_view(request, pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, "productdetail.html", {"product": product})

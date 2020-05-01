91abdf92fa837d4e6b91cc85c71c606c3bb79c49
unknown <elliot.redhead@gmail.com>
2020-05-01 21:23:26 +0100

    Adjusted heights of containers to fit display.

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,10 @@
 $(function () {
-    const viewportRemaining = window.innerHeight - $("nav").outerHeight();
+    const viewportHeight = window.innerHeight;
+    const headerHeight = $("nav").outerHeight();
+    const footerHeight = $("footer").outerHeight();
+    $(".main-container").first().css("min-height", viewportHeight - headerHeight - footerHeight);
+    // const viewportRemaining = window.innerHeight - $("nav").outerHeight();
     $(".jumbotron").css("margin-bottom", 0);
-    $(".jumbotron").outerHeight(viewportRemaining);
+    $(".jumbotron").outerHeight(viewportHeight - headerHeight);
 }
 );

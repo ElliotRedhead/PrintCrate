a979cfe9cc55196f2ed333000fd41a53d80e39e8
unknown <elliot.redhead@gmail.com>
2020-05-02 15:37:50 +0100

    Refactored function into standalone function.

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,10 +1,13 @@
 $(function () {
+    containerHeightHandling()
+}
+);
+
+function containerHeightHandling(){
     const viewportHeight = window.innerHeight;
     const headerHeight = $("nav").outerHeight();
     const footerHeight = $("footer").outerHeight();
     $(".main-container").first().css("min-height", viewportHeight - headerHeight - footerHeight);
-    // const viewportRemaining = window.innerHeight - $("nav").outerHeight();
     $(".jumbotron").css("margin-bottom", 0);
     $(".jumbotron").outerHeight(viewportHeight - headerHeight);
 }
-);

08098f2729a2783b22dfe9f55c9129f0a4fd3330
unknown <elliot.redhead@gmail.com>
2020-05-02 17:58:01 +0100

    Triggered request on cart item quantity change.

--- static/js/custom.js
+++ static/js/custom.js
@@ -11,3 +11,34 @@ function containerHeightHandling(){
     $(".jumbotron").css("margin-bottom", 0);
     $(".jumbotron").outerHeight(viewportHeight - headerHeight);
 }
+
+$(".quantity-input").change(function(){
+    newItemQuantity = this.value;
+    itemId = this.itemId;
+    const fetchInputData = {
+        itemId: itemId,
+        newItemQuantity: newItemQuantity 
+    };
+    fetch(".", fetchParameterSetup(fetchInputData))
+        .then(response => {
+            response.json()
+            .then(responseJson => {
+                console.log(responseJson)
+            })
+        })
+})
+
+function fetchParameterSetup(fetchInputData){
+	const fetchParameters = {
+		method: "POST",
+		cors: "*same-origin",
+		headers: new Headers({
+            "Content-Type": "application/json",
+            "Custom-Fetch": true,
+            "X-CSRFToken": csrftoken,
+            "X-Requested-With": "XMLHttpRequest"
+        }),
+		body: JSON.stringify(fetchInputData)
+	};
+	return fetchParameters;
+}

346c0062346e8f1e910381f041a000752ba7d5a0
unknown <elliot.redhead@gmail.com>
2020-05-03 21:45:01 +0100

    Corrected line length to meet suggested length.

--- contact/views.py
+++ contact/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def contact_us(request):
-    return render(request, "contact.html", {"page_title": "Contact Us | PrintCrate"})
+    return render(
+        request,
+        "contact.html",
+        {"page_title": "Contact Us | PrintCrate"}
+    )

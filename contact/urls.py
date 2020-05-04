c85af6220a98bde166065ed3705ae8d36e48582d
unknown <elliot.redhead@gmail.com>
2020-05-04 20:16:31 +0100

    Linked success view to URL.

--- contact/urls.py
+++ contact/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import contact_us
+from .views import contact_us, contact_success
 
 urlpatterns = [
-    path("", contact_us, name="contact")
+    path("", contact_us, name="contact"),
+    path("success/", contact_success, name="contact_success")
 ]

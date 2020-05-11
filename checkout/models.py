da09f1c10f0720075f90af676acb97ff7c48f22f
unknown <elliot.redhead@gmail.com>
2020-05-11 13:08:53 +0100

    Created checkout info form handling measures.

--- checkout/models.py
+++ checkout/models.py
@@ -1,10 +1,10 @@
+import datetime
 from django.db import models
-from products.models import Product
 from django.contrib.auth.models import User
 
 
 class CustomerShipping(models.Model):
-    customer_auth = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=100, blank=False)
     primary_address_line = models.CharField(max_length=50, blank=False)
     secondary_address_line = models.CharField(
@@ -14,7 +14,7 @@ class CustomerShipping(models.Model):
     postcode = models.CharField(max_length=20, blank=False)
     country = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
-    purchase_date = models.DateField()
+    purchase_date = models.DateField(default=datetime.date.today, blank=True)
 
     def __str__(self):
-        return f"{self.id}-{self.purchase_date}-{self.customer_auth.username}"
+        return f"{self.id}-{self.purchase_date}-{self.customer}"
--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import CustomerShippingForm
 
 
 @login_required
 def checkout_shipping_address_view(request):
-    return render(request, "checkout_shipping_address.html", {"page_title": "Shipping | PrintCrate", "customer_shipping_form": CustomerShippingForm})
+    if request.method == "POST":
+        customer_shipping = CustomerShippingForm(request.POST)
+        if customer_shipping.is_valid():
+            shipping = customer_shipping.save(commit=False)
+            shipping.customer = request.user
+            shipping.save()
+            return redirect("home")
+    else:
+        form = CustomerShippingForm()
+
+    return render(request, "checkout_shipping_address.html", {"page_title": "Shipping | PrintCrate", "customer_shipping_form": form})

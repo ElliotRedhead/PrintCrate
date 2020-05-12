fee61d86e50f23ba83aa2347d01015ae686ea5f0
unknown <elliot.redhead@gmail.com>
2020-05-12 21:54:15 +0100

    Double quoted variables for consistency.

--- checkout/forms.py
+++ checkout/forms.py
@@ -30,9 +30,9 @@ class PaymentForm(forms.Form):
         required=False
     )
     expiry_month = forms.ChoiceField(
-        label='Month', choices=MONTH_CHOICES, required=False
+        label="Month", choices=MONTH_CHOICES, required=False
     )
     expiry_year = forms.ChoiceField(
-        label='Year', choices=YEAR_CHOICES, required=False
+        label="Year", choices=YEAR_CHOICES, required=False
     )
     stripe_id = forms.CharField(widget=forms.HiddenInput())

6a4e18310d5f364cd302b8093686ed821774d306
unknown <elliot.redhead@gmail.com>
2020-05-12 21:55:41 +0100

    Created path URL for payment file.

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import checkout_shipping_address_view
+from .views import checkout_shipping_address_view, checkout_payment
 
 urlpatterns = [
-    path("info/", checkout_shipping_address_view, name="shipping")
+    path("info/", checkout_shipping_address_view, name="shipping"),
+    path("payment/", checkout_payment, name="payment"),
 ]

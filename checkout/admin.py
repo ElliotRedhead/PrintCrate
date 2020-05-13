2b63d2f63b3acd1b64b77a05aa721a2c25e497d3
unknown <elliot.redhead@gmail.com>
2020-05-13 23:42:39 +0100

    Registered order detail for admin view.

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import CustomerShipping
+from .models import CustomerShipping, OrderDetail
 
 admin.site.register(CustomerShipping)
+admin.site.register(OrderDetail)

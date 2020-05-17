fd3da320d1f1a46ab22b834cd31f80d5ef22024d
unknown <elliot.redhead@gmail.com>
2020-05-17 20:46:42 +0100

    Corrected line that was too long.

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,9 @@ from .views import registration, logout
 urlpatterns = [
     path("logout", logout, name="logout"),
     path("register", registration, name="register"),
-    path("login", auth.LoginView.as_view(template_name="login.html",
-                                         redirect_authenticated_user=True, extra_context={"page_title": "Login | PrintCrate"}), name="login"),
+    path("login", auth.LoginView.as_view(
+        template_name="login.html",
+        redirect_authenticated_user=True,
+        extra_context={"page_title": "Login | PrintCrate"}),
+        name="login"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

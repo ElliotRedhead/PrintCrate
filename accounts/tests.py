6c82594935f669c6c5a7b5a04dd11818406de7f3
unknown <elliot.redhead@gmail.com>
2020-05-17 20:20:09 +0100

    Constructed help text tests.

--- accounts/tests.py
+++ accounts/tests.py
@@ -12,3 +12,13 @@ class RegistrationFormTest(TestCase):
         self.assertTrue(form.fields["password1"].label == "Password")
         self.assertTrue(
             form.fields["password2"].label == "Password confirmation")
+
+    def test_field_help_prompts(self):
+        """Tests if Django help text tips are displayed for applicable fields, email field not included as validated via other means."""
+        form = UserRegisterForm(UserCreationForm)
+        self.assertTrue(form.fields["username"].help_text ==
+                        "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.")
+        self.assertTrue(form.fields["password1"].help_text ==
+                        "<ul><li>Your password can’t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can’t be a commonly used password.</li><li>Your password can’t be entirely numeric.</li></ul>")
+        self.assertTrue(form.fields["password2"].help_text ==
+                        "Enter the same password as before, for verification.")

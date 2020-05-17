110993593fa84dff2b949cfb48867266502a8209
unknown <elliot.redhead@gmail.com>
2020-05-17 15:00:53 +0100

    Documented about view.

--- about/views.py
+++ about/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def about_us(request):
+    """
+    Renders the "About Us" page.
+    """
     return render(request, "about.html", {"page_title": "About | PrintCrate"})

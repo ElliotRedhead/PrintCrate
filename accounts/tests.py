9a74cff0ad594e509cfd96d43f47a74a0947507c
unknown <elliot.redhead@gmail.com>
2020-05-17 17:34:16 +0100

    Tested form fields for existing and valid labels.

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .forms import UserRegisterForm
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your tests here.
+
+class RegistrationFormTest(TestCase):
+    def test_field_labels(self):
+        """Tests if field labels exist and are correct."""
+        form = UserRegisterForm(UserCreationForm)
+        self.assertTrue(form.fields["username"].label == "Username")
+        self.assertTrue(form.fields["email"].label == "Email")
+        self.assertTrue(form.fields["password1"].label == "Password")
+        self.assertTrue(
+            form.fields["password2"].label == "Password confirmation")

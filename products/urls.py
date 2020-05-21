c81d570404a7b476feb4211b38aa8cac334eaf1d
unknown <elliot.redhead@gmail.com>
2020-05-21 16:58:40 +0100

    Constructed product search feature.

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,5 @@ from .views import products_view, product_detail_view, product_search
 urlpatterns = [
     path("", products_view, name="products"),
     path("detail/<int:pk>", product_detail_view, name="product_detail"),
-    path("search/", product_search, name="product_search")
+    path("search", product_search, name="product_search")
 ]
--- products/views.py
+++ products/views.py
@@ -14,10 +14,11 @@ def product_detail_view(request, pk):
 
 
 def product_search(request):
-    search_query = ["baby", "phone"]
-    q_object = Q(name__icontains=search_query[0]) | Q(
-        description__icontains=search_query[0])
-    for term in search_query:
+    search_query = request.GET.get("search_query")
+    search_query_list = search_query.split(" ")
+    q_object = Q(name__icontains=search_query_list[0]) | Q(
+        description__icontains=search_query_list[0])
+    for term in search_query_list:
         q_object.add((Q(name__icontains=term) | Q(
             description__icontains=term)), q_object.connector)
     products = Product.objects.filter(q_object)

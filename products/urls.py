b8f09c72167b789b520a4a071444a187817661f4
unknown <elliot.redhead@gmail.com>
2020-05-21 16:00:30 +0100

    Constructed "or" search for products.

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import products_view, product_detail_view
+from .views import products_view, product_detail_view, product_search
 
 urlpatterns = [
     path("", products_view, name="products"),
-    path("detail/<int:pk>", product_detail_view, name="product_detail")
+    path("detail/<int:pk>", product_detail_view, name="product_detail"),
+    path("search/", product_search, name="product_search")
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Product
 
 
@@ -10,3 +11,14 @@ def products_view(request):
 def product_detail_view(request, pk):
     product = Product.objects.get(pk=pk)
     return render(request, "productdetail.html", {"product": product})
+
+
+def product_search(request):
+    search_query = ["baby", "phone"]
+    q_object = Q(name__icontains=search_query[0]) | Q(
+        description__icontains=search_query[0])
+    for term in search_query:
+        q_object.add((Q(name__icontains=term) | Q(
+            description__icontains=term)), q_object.connector)
+    products = Product.objects.filter(q_object)
+    return render(request, "productslist.html", {"products": products, "page_title": "Products | PrintCrate"})

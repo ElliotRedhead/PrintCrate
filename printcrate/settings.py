31172eba28ca338ce262045b8de62df198d4cf99
unknown <elliot.redhead@gmail.com>
2020-05-22 15:08:19 +0100

    Troubleshooting staticfiles on AWS.

--- printcrate/settings.py
+++ printcrate/settings.py
@@ -26,7 +26,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o8r+9a*9!7wmmfg=fm!r3mrv)p@hj@pbcl!^4zte9$-*#($*oq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if bool(os.getenv("DEPLOY")):
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "*.herokuapp.com",
                  "printcrate.herokuapp.com", "127.0.0.1"]
@@ -148,12 +151,11 @@ AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
 AWS_DEFAULT_ACL = None
+AWS_S3_FILE_OVERWRITE = False
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-if os.getenv("DEPLOY") == "False":
-    STATIC_URL = "/static/"
-else:
+if bool(os.getenv("DEPLOY")):
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     STATIC_URL = "/staticfiles/"
@@ -164,6 +166,8 @@ else:
     STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN,
                                      STATICFILES_LOCATION)
     MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+else:
+    STATIC_URL = "/static/"
 
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)

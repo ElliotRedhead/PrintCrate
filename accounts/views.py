2a91c07ce2d205b268f909726c210ddea4e1fb26
unknown <elliot.redhead@gmail.com>
2020-05-24 01:21:08 +0100

    Accessed the logged user's orders.

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 import sweetify
 from .forms import UserRegisterForm
+from checkout.models import OrderDetail, CustomerShipping
+from django.contrib.auth.models import User
 
 
 @login_required
@@ -38,4 +40,9 @@ def registration(request):
 
 
 def profile(request):
+    user = User.objects.get(username=request.user)
+    user_orders = OrderDetail.objects.filter(
+        shipping__customer_id=user.id)
+    for item in user_orders:
+        print(f"Orders ==== \n {item} \n ===")
     return render(request, "profile.html", {"page_title": "Profile | PrintCrate"})

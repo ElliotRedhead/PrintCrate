f2941ccdbfaeb7cbe5e9cec7ae610667e49376fe
unknown <elliot.redhead@gmail.com>
2020-05-25 15:34:19 +0100

    Custom form field decoration/placeholder info.

--- contact/forms.py
+++ contact/forms.py
@@ -2,11 +2,22 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    subject = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
+    subject = forms.CharField(
+        required=True,
+        label="Subject",
+        widget=forms.TextInput(attrs={"placeholder": "Enter Message Subject"}
+                               )
+    )
+    email = forms.EmailField(
+        required=True,
+        label="Email Address",
+        widget=forms.TextInput(attrs={"placeholder": "Your Email Address"})
+    )
     contact_message = forms.CharField(
         required=True,
+        label="Contact Message",
         widget=forms.Textarea(attrs={
+            "placeholder": "Enter your contact message here.",
             "rows": 6,
         }),
     )

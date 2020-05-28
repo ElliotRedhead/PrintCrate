17cc255032bd7efa1fdc5f4d666180fd58be7c29
unknown <elliot.redhead@gmail.com>
2020-05-28 22:48:28 +0100

    Reformatted for modularity & docstrings added.

--- checkout/views.py
+++ checkout/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from .forms import CustomerShippingForm, PaymentForm
 import sweetify
-import stripe
-from products.models import Product
-from .models import CustomerShipping, OrderDetail
-from cart.contexts import cart_contents
 from cart.views import empty_cart_modal
+from cart.contexts import cart_contents
+from products.models import Product
+from .forms import CustomerShippingForm, PaymentForm
+from .models import CustomerShipping, OrderDetail
+import stripe
 
 stripe.api_key = settings.STRIPE_SECRET
 
 
 @login_required
 def checkout_shipping_address_view(request):
+    """Constructs the shipping address information form.
+
+    Accessing user must be logged in with items in cart.
+    Shipping info is linked to the user but allows the user to
+    specify a new shipping address with each transaction if wanted.
+    """
     if not request.session.get("cart"):
         empty_cart_modal(request)
         return redirect("products")
@@ -26,18 +32,28 @@ def checkout_shipping_address_view(request):
             return redirect("payment")
     else:
         form = CustomerShippingForm()
-    return render(request, "checkout_shipping_address.html", {"page_title": "Shipping | PrintCrate", "customer_shipping_form": form})
+    return render(
+        request,
+        "checkout_shipping_address.html",
+        {"page_title": "Shipping | PrintCrate",
+         "customer_shipping_form": form}
+    )
 
 
 @login_required
 def checkout_payment(request):
+    """Conducts a test Stripe charge.
+
+    If payment is successful the order creation is triggered and
+    the user's cart is cleared.
+    """
     if request.method == "POST":
         payment_form = PaymentForm(request.POST)
         if payment_form.is_valid():
             cart = request.session.get("cart", {})
             cart_total = cart_contents(request)["total"]
             try:
-                customer = stripe.Charge.create(
+                stripe.Charge.create(
                     amount=int(cart_total*100),
                     currency="GBP",
                     description=request.user.email,
@@ -53,24 +69,30 @@ def checkout_payment(request):
                     request,
                     "Payment successful, thank you for your purchase."
                 )
-                for item, quantity in cart.items():
-                    product = get_object_or_404(
-                        Product, pk=item)
-                    product_total = quantity * product.price
-                    order_detail = OrderDetail(
-                        shipping=CustomerShipping.objects.filter(
-                            customer=request.user).last(),
-                        product=product,
-                        quantity=quantity,
-                        total=product_total,
-                    )
-                    order_detail.save()
+                create_order_product_records(request, cart)
+
                 del request.session["cart"]
                 return redirect(reverse("profile"))
     else:
         payment_form = PaymentForm()
-    return render(request, "checkout_payment.html", {
-        "page_title": "Payment | PrintCrate",
-        "payment_form": payment_form,
-        "publishable": settings.STRIPE_PUBLISHABLE
-    })
+    return render(
+        request, "checkout_payment.html",
+        {"page_title": "Payment | PrintCrate",
+         "payment_form": payment_form,
+         "publishable": settings.STRIPE_PUBLISHABLE}
+    )
+
+
+def create_order_product_records(request, cart):
+    """Creates the product order record in the database."""
+    for item, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item)
+        product_total = quantity * product.price
+        order_detail = OrderDetail(
+            shipping=CustomerShipping.objects.filter(
+                customer=request.user).last(),
+            product=product,
+            quantity=quantity,
+            total=product_total,
+        )
+        order_detail.save()

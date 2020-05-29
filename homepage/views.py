8b308051e4c17caf3919770e83643b112cb7be06
unknown <elliot.redhead@gmail.com>
2020-05-30 00:12:41 +0100

    Selected 3 random showcase products for homepage.

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from products.models import Product
+import random
 
 
 def home(request):
-    products = Product.objects.filter(showcase_product=True)
+    showcase_products = Product.objects.filter(
+        showcase_product=True)
+    total_showcase_products = showcase_products.count()
+    random_indices = random.Random().sample(range(0, total_showcase_products), 3)
+    products = []
+    for index in random_indices:
+        products.append(showcase_products.all()[index])
     return render(request, "home.html", {"page_title": "Home | PrintCrate", "products": products})

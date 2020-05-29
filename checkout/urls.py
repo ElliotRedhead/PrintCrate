0e86ac1d468aff0388e82d673cb6aa59cb88f641
unknown <elliot.redhead@gmail.com>
2020-05-29 08:47:12 +0100

    Reassigned URL for better semantics.

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import checkout_shipping_address_view, checkout_payment
 
 urlpatterns = [
-    path("info/", checkout_shipping_address_view, name="shipping"),
+    path("shipping/", checkout_shipping_address_view, name="shipping"),
     path("payment/", checkout_payment, name="payment"),
 ]

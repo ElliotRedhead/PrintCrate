e2077f1bee331f7ac2831362b3eac7d43e00404c
unknown <elliot.redhead@gmail.com>
2020-05-31 21:04:17 +0100

    Skipped sampling if only three showcase products.

--- homepage/views.py
+++ homepage/views.py
@@ -9,12 +9,14 @@ def home(request):
         showcase_product=True)
     products = []
     total_showcase_products = showcase_products.count()
-    if total_showcase_products >= 3:
+    if total_showcase_products > 3:
         random_indices = random.Random().sample(
             range(0, total_showcase_products), 3
         )
         for index in random_indices:
             products.append(showcase_products.all()[index])
+    elif total_showcase_products == 3:
+        products = showcase_products
     return render(request, "home.html", {
         "page_title": "Home | PrintCrate",
         "products": products

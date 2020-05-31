01e866f0584bfcd9c478d5af9a7fb564bbb54ad6
unknown <elliot.redhead@gmail.com>
2020-05-31 20:46:21 +0100

    Fixed breaking bug:min showcase products required.

--- homepage/views.py
+++ homepage/views.py
@@ -7,13 +7,14 @@ def home(request):
     """Renders the homepage with random selection from showcase products."""
     showcase_products = Product.objects.filter(
         showcase_product=True)
-    total_showcase_products = showcase_products.count()
-    random_indices = random.Random().sample(
-        range(0, total_showcase_products), 3
-    )
     products = []
-    for index in random_indices:
-        products.append(showcase_products.all()[index])
+    total_showcase_products = showcase_products.count()
+    if total_showcase_products >= 3:
+        random_indices = random.Random().sample(
+            range(0, total_showcase_products), 3
+        )
+        for index in random_indices:
+            products.append(showcase_products.all()[index])
     return render(request, "home.html", {
         "page_title": "Home | PrintCrate",
         "products": products

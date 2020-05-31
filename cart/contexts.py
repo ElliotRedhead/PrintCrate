d05446be10ce2a8f1a7d0afe251e7487337d9d61
unknown <elliot.redhead@gmail.com>
2020-05-31 11:52:33 +0100

    Validated customer's cart against product stock.

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,4 +1,9 @@
 from products.models import Product
+import sweetify
+
+
+def database_stock_check(cart_quantity, product_availability):
+    assert cart_quantity <= product_availability
 
 
 def cart_contents(request):
@@ -14,16 +19,29 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
+    cart_adjusted = False
     for item, quantity in list(cart.items()):
         try:
             product = Product.objects.get(pk=item)
-            total += quantity * product.price
-            product_count += quantity
-            cart_items.append(
-                {"id": item, "quantity": quantity, "product": product})
+            try:
+                database_stock_check(quantity, product.stock_available)
+            except AssertionError:
+                cart_adjusted = True
+                quantity = product.stock_available
+                if quantity == 0:
+                    cart.pop(item)
+                request.session["cart"] = cart
+            finally:
+                total += quantity * product.price
+                product_count += quantity
+                cart_items.append(
+                    {"id": item, "quantity": quantity, "product": product})
         except Product.DoesNotExist:
+            cart_adjusted = True
             cart.pop(item)
             request.session["cart"] = cart
+    if cart_adjusted:
+        print("The user's cart has been changed due to quantity restrictions.")
     return {
         "cart_items": cart_items,
         "total": total,
